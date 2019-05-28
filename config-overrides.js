33200a66de7fd746f254430bf86aa81f1e272ee7
CycloneBoy <xuanfeng1992@gmail.com>
2019-05-29 00:28:58 +0800

    antd 定制主题

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,15 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 module.exports = override(
        fixBabelImports('import', {
              libraryName: 'antd',
          libraryDirectory: 'es',
-         style: 'css',
+         style: true,
        }),
+        addLessLoader({
+               javascriptEnabled: true,
+           modifyVars: {
+                   '@primary-color': '#1DA57A'
+           },
+        }),
 );
