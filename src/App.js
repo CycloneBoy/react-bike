5d29e2a6bbe197a5f25f579bdec3118d136cec00
CycloneBoy <xuanfeng1992@gmail.com>
2019-05-28 23:55:48 +0800

    项目初始化

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from 'antd/lib/button';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+        <div className="App">
+          <Button type="primary">Button</Button>
+        </div>
+    );
+  }
+}
+
+export default App;
