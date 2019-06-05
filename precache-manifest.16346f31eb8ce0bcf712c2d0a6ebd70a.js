2c9a622512d6de5ccfe6c4e14eb30a69cd224017
CycloneBoy <xuanfeng1992@gmail.com>
2019-06-05 23:42:44 +0800

    Updates

---
+++ precache-manifest.16346f31eb8ce0bcf712c2d0a6ebd70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b6fff4851bd4551c998fe5647ded3bf",
+    "url": "/react-bike/index.html"
+  },
+  {
+    "revision": "dbf7c61991cc70d568e5",
+    "url": "/react-bike/static/css/2.c9274424.chunk.css"
+  },
+  {
+    "revision": "dda9f70017f419218f05",
+    "url": "/react-bike/static/css/main.d6e3773d.chunk.css"
+  },
+  {
+    "revision": "dbf7c61991cc70d568e5",
+    "url": "/react-bike/static/js/2.f2279d5b.chunk.js"
+  },
+  {
+    "revision": "dda9f70017f419218f05",
+    "url": "/react-bike/static/js/main.e6bee9dd.chunk.js"
+  },
+  {
+    "revision": "3794c36a4ed68e774902",
+    "url": "/react-bike/static/js/runtime~main.3a1c3d06.js"
+  }
+]);
